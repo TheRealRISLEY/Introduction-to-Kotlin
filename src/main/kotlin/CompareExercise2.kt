990c178cb130eb800b50e218d400e4dc7a76bfbf
luthor628 <abdulrahmanabdulaziz628@gmail.com>
2023-03-15 12:27:48 +0300

    Comparison

---
+++ src/main/kotlin/CompareExercise2.kt
@@ -0,0 +1,37 @@
+fun main(args: Array<String>) {
+    print("Enter Number")
+    var number:Int = readln()!!.toInt()
+
+    if (number == 1)
+    {
+        print("Data Deals")}
+
+    else if (number == 2){
+        print("Daily Bundles")}
+
+    else if (number == 3){
+        print("Weekly Bundles")}
+
+    else if (number == 4){
+        print("GO MONTHLY")}
+
+    else if (number == 5){
+        print("No Expiry")}
+
+    else if (number == 6){
+        print("Video Bundles")}
+
+    else if (number == 7){
+        print("Okoa Data")}
+
+    else if (number == 8){
+        print("Lipa Mdogo")}
+
+    else if (number == 9){
+        print("Hot Minutes")}
+
+    else  {
+        print("Invalid Number")}
+
+
+}
