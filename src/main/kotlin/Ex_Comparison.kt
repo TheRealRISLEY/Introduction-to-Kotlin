7ac2128e0c3772448e440b3cad9d09eb8f2a4b4a
luthor628 <abdulrahmanabdulaziz628@gmail.com>
2023-03-15 13:22:31 +0300

    Exercise

---
+++ src/main/kotlin/Ex_Comparison.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+    print("Enter Your Marks")
+    var marks:Int = readln()!!.toInt()
+
+    if (marks >= 80 )
+    {
+        print("A - Congratulations")}
+
+    else if (marks >= 60){
+        print("B - Good Trial")}
+
+    else if (marks >= 40){
+        print("C - Good")}
+
+    else if (marks >= 20){
+        print("D - Trial")}
+
+    else if (marks >= 10){
+        print("E - You have to Repeat")}
+
+
+
+}
